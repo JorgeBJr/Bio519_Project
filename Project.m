ff892512827d8d6838bbbfe1ec739b7e2750d703
JorgeBJr <jorgebjr@uw.edu>
2016-03-03 22:34:34 -0800

    Adding in the plot
    This is so I can visually display the centroid position (for internal
    purposes only)

--- Project.m
+++ Project.m
@@ -6,3 +6,10 @@ data = csvread('811m_Centerxypts.csv',1,0); %This will import the CSV file
 %with the appropriate offset that imports the two columns without the
 %headers.
 
+Centroid_X = data(:,1);
+Centroid_Y = data(:,2);
+
+figure;
+plot(Centroid_X',Centroid_Y','.','MarkerSize',20)
+xlabel('x position')
+ylabel('y position')
