2ba9a760c869bed56102781c5ee37884dad5f8b2
JorgeBJr <jorgebjr@uw.edu>
2016-03-03 21:55:27 -0800

    Adding in dataset
    added in dataset (that is not published here because I will not allow
    the public to crowdsource this data), and how to import the data

--- Project.m
+++ Project.m
@@ -2,3 +2,7 @@
 clear all; close all; clc;
 
 %import stuff
+data = csvread('811m_Centerxypts.csv',1,0); %This will import the CSV file
+%with the appropriate offset that imports the two columns without the
+%headers.
+
