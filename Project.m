31c13fceabb09bde45b75935476d4f474aaf36c7
JorgeBJr <jorgebjr@uw.edu>
2016-03-07 10:57:22 -0800

    Time domain addition
    Added in Time domain stuff and prepping for FFT

--- Project.m
+++ Project.m
@@ -2,11 +2,11 @@
 clear all; close all; clc;
 
 %import stuff
-data_centroid = csvread('811m_Centerxypts.csv',1,0); %This will import the CSV file
-%of the centroid (i.e. input stimulus) with the appropriate offset that 
-%imports the two columns without the headers. 
-data_abdomen = csvread('811m_completexypts.csv',1,0); %This will import the CSV 
-%file of the abdomen (i.e. output response) with the appropriate offset 
+data_centroid = csvread('811m_Centerxypts.csv',1,0); %This will import the 
+%CSV file of the centroid (i.e. input stimulus) with the appropriate offset
+%that imports the two columns without the headers. 
+data_abdomen = csvread('811m_completexypts.csv',1,0); %This will import the 
+%CSV file of the abdomen (i.e. output response) with the appropriate offset 
 %that imports the two columns without the headers. 
 
 Centroid_X = data_centroid(:,1);
@@ -20,5 +20,26 @@ hold on;
 plot(Abdomen_X',Abdomen_Y','.','MarkerSize',20)
 xlabel('x position')
 ylabel('y position')
+%Frequency is 3 Hz? Check with Brandon
 
-%Frequency is 3 Hz?
+%The following five lines of code are from 
+%http://www.mathworks.com/help/matlab/ref/fft.html 
+Fs = 3; %frequency is 3 Hertz
+T = 1/Fs; %This is the period
+L = 1000; %This is the length of the signal since our data sets are arrays 
+%of 1000 x 1
+t = (0:L-1)*T; %This is how we get our time vector.
+
+
+%Finding theta by determining the difference between the input and output
+deltaX = Centroid_X - Abdomen_X;
+deltaY = Centroid_Y - Abdomen_Y;
+deltaFraction = deltaY./deltaX;
+theta = atand(deltaFraction); %This returns the arctan of the function 
+%IN DEGREES
+
+%Because I want to see the theta with respect to time...
+figure;
+plot(t,theta,'.','MarkerSize',20)
+xlabel('Time (in seconds)')
+ylabel('Theta (in degrees)')
