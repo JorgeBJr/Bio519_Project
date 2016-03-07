23e2c54bcbf83a48e075aeffdc15eb552293a46c
JorgeBJr <jorgebjr@uw.edu>
2016-03-07 10:20:15 -0800

    Added in abdomen data
    Abdomen data is inserted so that I can see both the input stimulus
    (centroid) and output response (abdomen).
    Man, this is starting to look good.

--- Project.m
+++ Project.m
@@ -2,14 +2,23 @@
 clear all; close all; clc;
 
 %import stuff
-data = csvread('811m_Centerxypts.csv',1,0); %This will import the CSV file
-%with the appropriate offset that imports the two columns without the
-%headers.
+data_centroid = csvread('811m_Centerxypts.csv',1,0); %This will import the CSV file
+%of the centroid (i.e. input stimulus) with the appropriate offset that 
+%imports the two columns without the headers. 
+data_abdomen = csvread('811m_completexypts.csv',1,0); %This will import the CSV 
+%file of the abdomen (i.e. output response) with the appropriate offset 
+%that imports the two columns without the headers. 
 
-Centroid_X = data(:,1);
-Centroid_Y = data(:,2);
+Centroid_X = data_centroid(:,1);
+Centroid_Y = data_centroid(:,2);
+Abdomen_X = data_abdomen(:,1);
+Abdomen_Y = data_abdomen(:,2);
 
 figure;
 plot(Centroid_X',Centroid_Y','.','MarkerSize',20)
+hold on;
+plot(Abdomen_X',Abdomen_Y','.','MarkerSize',20)
 xlabel('x position')
 ylabel('y position')
+
+%Frequency is 3 Hz?
